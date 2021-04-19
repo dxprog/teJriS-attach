f33a37e755c9e7da5afc967ddd104cf2dea5a0c2
Matt Hackmann <matt@dxprog.com>
2021-04-19 00:14:00 -0700

    removed all traces of jelda (maybe)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { NumDict, StringDict } from './interfaces/common';
 import Canvas from './graphics/canvas';
-import Player from './game/player';
-import Dungeon from './game/dungeon';
 
 // The number of milliseconds between each frame
 const FRAME_DELAY: number = Math.floor(1000 / 60);
@@ -42,7 +40,6 @@ const KEY_STATE: StringDict<boolean> = {
 class Game {
   private _win: Window;
   private _doc: Document;
-  private _canvasEl: HTMLCanvasElement;
   private _ctx: CanvasRenderingContext2D;
   private _buffer: HTMLCanvasElement;
   private _canvas: Canvas;
@@ -51,7 +48,6 @@ class Game {
   constructor(window: Window, canvasEl: HTMLCanvasElement) {
     this._win = window;
     this._doc = window.document;
-    this._canvasEl = canvasEl;
     this._ctx = canvasEl.getContext('2d');
     this._ctx.imageSmoothingEnabled = false;
 
@@ -59,11 +55,6 @@ class Game {
     this._buffer.width = BUFFER_WIDTH;
     this._buffer.height = BUFFER_HEIGHT;
     this._canvas = new Canvas(this._buffer);
-
-    this._gameObjects = {
-      player: new Player(this),
-      dungeon: new Dungeon(require('./data/dungeon1'), this)
-    };
 
     this._doc.addEventListener('keydown', this._handleKeydown.bind(this));
     this._doc.addEventListener('keyup', this._handleKeyup.bind(this));
@@ -78,10 +69,7 @@ class Game {
     this._canvas.clear();
 
     this._canvas.beginRender();
-    this._gameObjects.dungeon.update();
-    this._gameObjects.player.update(KEY_STATE);
-    this._gameObjects.dungeon.draw();
-    this._gameObjects.player.draw();
+
     this._canvas.endRender();
 
     this._ctx.drawImage(this._buffer, 0, 0, BUFFER_WIDTH, BUFFER_HEIGHT,
